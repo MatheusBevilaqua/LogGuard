6cf928080903bb3c7fab0fe1e887e7755e58cbdc
Gustavo Cardoso Sousa <gustavo.sousa@sptech.school>
2024-09-06 18:57:27 -0300

    Partes comentadas do Python
    Foram comentadas as partes que fazem a inserção no banco.

--- extras/Python/Captura-Dados-LogGuard.py
+++ extras/Python/Captura-Dados-LogGuard.py
@@ -1,56 +1,56 @@
 # Imports de módulos externos necessários para manipular tempo,capturar dados e conectar com o BD MySQL
 import time
 import psutil as ps
-from mysql.connector import connect, Error
+# from mysql.connector import connect, Error
 
 def main(): # Função principal, fiz assim por questão de organização
     imprimeMenu("Olá, Bem vindo(a)! Aqui está a captura de dados em tempo real de: Uso de CPU, Uso de RAM, Uso de Disco, Conexões Abertas em Rede, e Tempo de atividade!")
-    escolha = input("Você deseja que os dados capturados sejam inseridos no Banco de Dados (S/N)? ")
-    habilitarInsercao = False
+    # escolha = input("Você deseja que os dados capturados sejam inseridos no Banco de Dados (S/N)? ")
+    # habilitarInsercao = False
     time.sleep(2.5)
     print("\n------------------ USO DE HARDWARE ------------------\n")
     print(formataLinha('CPU (%)', 'RAM (%)', 'Conexões', 'Tempo Atividade'))
     print('-' * 60)
 
-    if(escolha.lower() == "s"):
-        habilitarInsercao = True
+    # if(escolha.lower() == "s"):
+    #     habilitarInsercao = True
 
     while True:
         # Coleta e exibe os dados do sistema
         dados = obterDadosSistema()
         # Caso o usuário queira, insere no banco de dados aqui:
-        if (habilitarInsercao):
-            query = montaQuery(*dados)
-            executarQuery(query)
+        # if (habilitarInsercao):
+        #     query = montaQuery(*dados)
+        #     executarQuery(query)
         # Imprime os dados na tabela
         print(formataLinha(*dados)) # Utilizando o "*" como forma de "expandir" a tupla em dados separados, pois se passasse sem isso, ficaria apenas 1 tupla inteira como parâmetro.
         # Aguarda 2 segundos antes de atualizar
         time.sleep(2)
 
-def executarQuery(script): # Função responsável por inserir os dados no banco, recebe uma query SQL qualquer como parâmetro e a executa, usando as credenciais específicas
-    config = {
-      'user': 'root',
-      'password': '2205',
-      'host': 'localhost',
-      'database': 'Python'
-    }
-
-    try:
-      db = connect(**config)
-      if db.is_connected():
-        db_info = db.get_server_info()
-        # print('Connected to MySQL server version -', db_info)
-
-        with db.cursor() as cursor:
-          cursor.execute(script)
-          db.commit()  # Confirma a transação no banco de dados
-          # print("Dados inseridos com sucesso!")
-
-        cursor.close()
-        db.close()
-
-    except Error as e:
-      print('Erro ao conectar com o MySQL -', e)
+# def executarQuery(script): # Função responsável por inserir os dados no banco, recebe uma query SQL qualquer como parâmetro e a executa, usando as credenciais específicas
+#     config = {
+#       'user': 'root',
+#       'password': '2205',
+#       'host': 'localhost',
+#       'database': 'Python'
+#     }
+#
+#     try:
+#       db = connect(**config)
+#       if db.is_connected():
+#         db_info = db.get_server_info()
+#         # print('Connected to MySQL server version -', db_info)
+#
+#         with db.cursor() as cursor:
+#           cursor.execute(script)
+#           db.commit()  # Confirma a transação no banco de dados
+#           # print("Dados inseridos com sucesso!")
+#
+#         cursor.close()
+#         db.close()
+#
+#     except Error as e:
+#       print('Erro ao conectar com o MySQL -', e)
 
 def imprimeMenu(texto): # Função responsável por imprimir o menu inicial, contém o nosso nome em ASCII ART, recebe os parâmetros de: texto a ser impresso e tempo a esperar após imprimir
     print(""" 
@@ -97,8 +97,8 @@ def obterDadosSistema(): # Aqui, colho os dados por componente separadamente:
 
     return (porcentagemCpu, porcentagemMemoria, conexoesRede, tempoAtividadeStr) # Retorno da função, a tupla com todos os dados em ordem.
 
-def montaQuery(*dados):
-    return "INSERT INTO DadoCapturado (`CPU%`, `RAM%`, `ConexoesRede`, `TempoAtividade`) VALUES ('{}', '{}', '{}', '{}')".format(*dados)
+# def montaQuery(*dados):
+#     return "INSERT INTO DadoCapturado (`CPU%`, `RAM%`, `ConexoesRede`, `TempoAtividade`) VALUES ('{}', '{}', '{}', '{}')".format(*dados)
 
 # Como quis rodar o arquivo pela função "main()", preciso fazer isso:
 # A variável "__name__" é uma variável que tem seu valor atribuído automaticamente pelo Python.
